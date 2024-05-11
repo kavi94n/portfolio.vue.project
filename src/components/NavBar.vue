31cf11c0d032aadb718a4024158f9fefc473fc2e
kavi94n <kavirajh@hotmail.com>
2024-05-11 17:30:19 +0200

    change

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  
+    <v-app-bar app color="black" dark flat class="px-12">
+      <v-btn>
+        <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon>KAVI
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text @click="scroll('ome')" class="text-yellow">Home</v-btn>
+      <v-btn text @click="scroll('about')" class="text-yellow">About</v-btn>
+      <v-btn text @click="scroll('portfolio')" class="text-yellow">Portfolio</v-btn>
+      <v-btn text @click="scroll('services')" class="text-yellow">Services</v-btn>
+      <v-btn text @click="scroll('page')" class="text-yellow">Page</v-btn>
+      <v-btn text @click="scroll('contact')" class="text-yellow">Contact</v-btn>
+    </v-app-bar>
+  
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({behavior: "smooth"});
+  }
+}
+
+
+}
+  
+</script>
+
+<style>
+
+</style>
